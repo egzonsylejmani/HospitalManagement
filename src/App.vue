c057b3603d1ef825392933cce2adb4fb11deccf7
EgzonArifiii <72439549+EgzonArifiii@users.noreply.github.com>
2023-03-12 17:02:16 +0100

    Added the footer
    Fixed some bugs there as well

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue'
+import footer from './components/footer.vue'
 </script>
 
 <template>
 <Navbar />
   <RouterView />
+  <footer />
+
 </template>
